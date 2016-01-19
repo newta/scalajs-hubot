d7d9133feb8f2af064b19179762bc871f01aa5ed
shinden <t.newtaro@gmail.com>
2016-01-19 15:59:03 +0900

    Add initial project

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "scalajs-com.github.newta.scalajs.hubot"
+
+version := "1.0"
+
+enablePlugins(ScalaJSPlugin)
+
+name := "Scala.js Tutorial"
+
+scalaVersion := "2.11.7"
+
---
+++ src/main/scala/com.github.newta.scalajs.hubot/ScalaRobot.scala
@@ -0,0 +1,10 @@
+package com.github.newta.scalajs.hubot
+
+
+import scala.scalajs.js.JSApp
+
+object ScalaRobt extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
